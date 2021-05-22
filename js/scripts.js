758a52c2dd2690c881b740f446c323e375576eb2
Elijah Wilcott <ejw773@gmail.com>
2021-05-22 11:02:41 -0400

    Completes Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,10 @@ $(function() {
 
         }
     });
+    $("#reserveButton").click(function() {
+        $("#reserveModal").modal('show');
+    });
+    $("#loginButton").click(function() {
+        $("#loginModal").modal('show');
+    })
 });
